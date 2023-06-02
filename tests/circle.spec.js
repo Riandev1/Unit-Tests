b036ae29d8bd7bf9b319e45c7a4a18835cdc8519
oDevSilva <ocontato.dev@gmail.com>
2023-06-02 15:40:29 -0300

    Feito o requisito 04

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -13,7 +13,7 @@ const circle = require('../src/circle');
   Comportamento:
     - circle(1) // Retorno: {radius: 1, area: 3.14, circumference: 6.28}
     - circle(7) // Retorno: {radius: 7, area: 153.86, circumference: 43.96}
-    - circle(3) // Retorno: {radius: 3, area: 28,26, circumference: 18.84}
+    - circle( 3) // Retorno: {radius: 3,area: 28,26, circumference: 18.84}
 
   DICA: Números de ponto flutuante em JavaScript são imprecisos!  Para testar, vá no console do navegador e faça `0.2 + 0.1`.
         Uma solução pra isso pode ser fazer a soma no seguinte formato: `parseFloat((0.2 + 0.1).toPrecision(2))`.
@@ -23,14 +23,22 @@ const circle = require('../src/circle');
 
 describe('4 - Implemente os casos de teste para a função `circle`', () => {
   it('Verifica se ao receber um raio, a função `circle` retorna um objeto contendo os valores esperados', () => {
-    fail('Teste vazio!');
+    
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna undefined, caso o parâmetro passado não seja um número.
+    expect(circle('2')).toBeUndefined();
     // Teste se circle retorna um objeto.
+    expect(typeof circle(2)).toBe('object')
     // Teste se o objeto retornado possui 3 propriedades.
+    expect(circle(2)).toHaveProperty('radius', 'area', 'circumference')
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(circle()).toBeUndefined();
     // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à circunferência correta para um círculo de raio 2.
+    expect(circle(2).circumference).toEqual(12.56)
     // Teste se dentro do objeto retornado, a função retorna uma `key` com `value` igual à área correta para um círculo de raio 3.
+    expect(circle(3).area).toEqual(28.259999999999998)
     // Teste se a função retorna, em um objeto, os dados corretos de um círculo de raio 3.
+    expect(circle(3)).toEqual({ radius: 3, area: 28.259999999999998, circumference: 18.84});
   });
-});
+  });
+
