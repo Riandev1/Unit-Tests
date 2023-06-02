5dedc10339b3f61b8ec3e80a07c70df762363bbf
oDevSilva <ocontato.dev@gmail.com>
2023-06-02 16:58:23 -0300

    Feito o requisito 08

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,14 +16,17 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+  const result = [];
+  
+  for (let i = 0; i <= 3; i += 1) {
+    result.push(i);
+    
+    for (let j = 2; j <= 3; j += 1) {
+      result.push(j);
     }
   }
-  return myArray;
+  
+  return result;
 };
 
 module.exports = myCounter;
